cc378c969c65ba02830d388cc6bfbd9317d4a79b
loko06320 <montet.steve@gmail.com>
2018-07-05 05:07:47 +0200

    Update main.lua

--- client/main.lua
+++ client/main.lua
@@ -89,24 +89,6 @@ AddEventHandler('esx_illegalshops:hasExitedMarker', function(zone)
 end)
 
 --SPAWN PNJ
-local GUI                       = {}
-GUI.Time                        = 0
-local generalLoaded 		        = false
-
-Citizen.CreateThread(function()
-  while ESX == nil do
-    TriggerEvent('esx:getSharedObject', function(obj) ESX = obj end)
-    Citizen.Wait(1)
-  end
-end)
-
-RegisterNetEvent('esx:playerLoaded')
-AddEventHandler('esx:playerLoaded', function(xPlayer)
-  PlayerData = xPlayer
-  CreateWeed()
-end)
-
-
 local PNJ = {
 	{id=1, Name=PnjWeed, VoiceName="GENERIC_INSULT_HIGH", Ambiance="AMMUCITY", Weapon=1649403952, modelHash="g_m_m_chicold_01", x = 182.35203552246, y = -938.38549804688, z = 29.091911315918, heading=53.4745941},
 }
@@ -128,6 +110,8 @@ Citizen.CreateThread(function()
       PNJ[i].id = CreatePed(28, PNJ[i].modelHash, PNJ[i].x, PNJ[i].y, PNJ[i].z, PNJ[i].heading, false, false)
       TaskStartScenarioInPlace(PNJ[i].id,'WORLD_HUMAN_DRUG_DEALER', 0 , false )
     end
+	end
+  generalLoaded = true
 end)
 
 -- Create Blips
