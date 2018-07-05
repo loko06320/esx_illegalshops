784a49af60cf568ef8a51f6b78bcc6d09c593ce4
loko06320 <montet.steve@gmail.com>
2018-07-05 04:44:24 +0200

    Update esx_shops.sql

--- esx_shops.sql
+++ esx_shops.sql
@@ -1,6 +1,6 @@
 USE `essentialmode`;
 
-CREATE TABLE `shops` (
+CREATE TABLE `illegalshops` (
 	`id` int(11) NOT NULL AUTO_INCREMENT,
 	`store` varchar(100) NOT NULL,
 	`item` varchar(100) NOT NULL,
@@ -9,11 +9,6 @@ CREATE TABLE `shops` (
 	PRIMARY KEY (`id`)
 );
 
-INSERT INTO `shops` (store, item, price) VALUES
-	('TwentyFourSeven','bread',30),
-	('TwentyFourSeven','water',15),
-	('RobsLiquor','bread',30),
-	('RobsLiquor','water',15),
-	('LTDgasoline','bread',30),
-	('LTDgasoline','water',15)
+INSERT INTO `illegalshops` (store, item, price) VALUES
+	('ballas','handcuff',30)
 ;
